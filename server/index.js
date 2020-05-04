7975d7a27411f623c88cc5c954505e701fca9d3e
ildanaruzybayeva <ildana.ruzybayeva@gmail.com>
2020-05-04 17:13:02 +0200

    cancel

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my api');
+})
+
 
 const oauth2Client = new OAuth2(
     process.env.clientId, // ClientID
@@ -14,27 +28,36 @@ oauth2Client.setCredentials({
 });
 const accessToken = oauth2Client.getAccessToken()
 
-const smtpTransport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        type: "OAuth2",
-        user: "ildana.ruzybayeva@gmail.com",
-        clientId: process.env.clientId,
-        clientSecret: process.env.clientSecret,
-        refreshToken: process.env.refreshToken,
-        accessToken: accessToken
-    }
-});
 
-const mailOptions = {
-    from: "ildana.ruzybayeva@gmail.com",
-    to: "ildvnv@gmail.com",
-    subject: "Node.js Email with Secure OAuth",
-    generateTextFromHTML: true,
-    html: "<b>test</b>"
-};
+app.post('/api', (req, res) => {
+    const data = req.body;
 
-smtpTransport.sendMail(mailOptions, (error, response) => {
-    error ? console.log(error) : console.log(response);
-    smtpTransport.close();
-});
+    const smtpTransport = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            type: "OAuth2",
+            user: "ildana.ruzybayeva@gmail.com",
+            clientId: process.env.clientId,
+            clientSecret: process.env.clientSecret,
+            refreshToken: process.env.refreshToken,
+            accessToken: accessToken
+        }
+    });
+
+    const mailOptions = {
+        from: "ildana.ruzybayeva@gmail.com",
+        to: `${data.email}`,
+        subject: "hello ily",
+        generateTextFromHTML: true,
+        html: `
+        <p>${data.email}</p>
+        <p>${data.message}</p>`
+    };
+
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+        error ? res.send(error) : res.send(response);
+        smtpTransport.close();
+    });
+})
+
+app.listen(3000, () => console.log(`Server started on 3000`));
